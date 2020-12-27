0941cf8f119b463f0330154b2801dafd2fc5d027
AkilaNilmalgoda <akilanilmalgoda2@gmail.com>
2020-12-28 01:15:04 +0300

    HW-css updated

--- src/components/HWs/HW.js
+++ src/components/HWs/HW.js
@@ -33,21 +33,21 @@ const HW = ({match}) => {
                     <h1 >
                         {item.title}
                     </h1>
-                    <div className="container main-hw-box py-3">
-                        <div className="row col-12">
-                            <div className="col-9">
-                                <h3 className='pb-3'>Lesson</h3>
-                                <div className='video' >
+                    <div className="container main-hw-box py-3 px-0">
+                        <div className="row d-flex justify-content-center col-12 mx-auto p-0"> 
+                            <div className="col-lg-9 col-sm-12 pb-2">
+                                <h3 className='py-3'>Lesson</h3>
+                                <div className='video col-12' >
                                     <ReactPlayer controls  url={item.video} />
                                 </div>
                             </div>
-                            <div className="col-3">
-                                <h3 className='pb-3'>Homework</h3>
-                                <div>
+                            <div className="col-lg-3 col-sm-12 col-12 mx-auto ">
+                                <h3 className='py-3'>Homework</h3>
+                                <div className= 'mx-auto'>
                                     {item.worksheets.map((worksheet,id)=> {
                                         return(
-                                            <div className='pb-1' key={id}>
-                                                <a href={worksheet.link} target="_blank" rel="noreferrer noopener"> <button className='worksheet-download-btn'>Worksheet {worksheet.id}</button></a>
+                                            <div className='pb-1 col-12 mx-auto' key={id}>
+                                                <a href={worksheet.link} target="_blank" rel="noreferrer noopener"> <button className='worksheet-download-btn col-12 mx-auto'>Worksheet {worksheet.id}</button></a>
                                             </div>
                                         )
                                     })}
